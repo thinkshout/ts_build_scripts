381226d7b2b4726cad618b0d3928857837745958
Greg Boggs <greg.boggs@thinkshout.com>
2015-11-27 11:09:51 -0800

    Add scripts from ts_build and update them to move config.sh to the root of the profile.

---
+++ default.config.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# match your install profile name
+PROJECT=ts_build
+
+# pantheon, acquia, generic
+HOSTTYPE=pantheon
+
+SITENAME="Site Name"
+
+# Copy the git repository URL from the Pantheon dashboard.
+GITREPO=
